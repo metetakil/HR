c115bd385d4bc42e561ff4085da029791986e9c7
metetakil <mtakil@gmail.com>
2019-06-30 23:00:15 +0100

    Contact fix

--- src/main/java/com/mtakil/hr/entity/Contact.java
+++ src/main/java/com/mtakil/hr/entity/Contact.java
@@ -5,14 +5,5 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public class Contact extends BaseEntity {
 
-	protected Employee employee;
 
-	public Employee getEmployee() {
-		return employee;
-	}
-
-	public void setEmployee(Employee employee) {
-		this.employee = employee;
-	}
-	
 }
