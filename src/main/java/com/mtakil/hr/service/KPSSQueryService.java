c20b18141bd3b66044113f02065b202cd4dec88e
Mete Takil <m.takil@EDI.LOCAL>
2019-07-09 12:25:55 +0100

    Logging improved

--- src/main/java/com/mtakil/hr/service/KPSSQueryService.java
+++ src/main/java/com/mtakil/hr/service/KPSSQueryService.java
@@ -3,9 +3,9 @@ package com.mtakil.hr.service;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,9 +14,8 @@ import com.mtakil.hr.model.ExamResult;
 @Service
 public class KPSSQueryService {
 
-	//@Autowired
-	//private RestTemplate restTemplate;
-	
+	private static final Logger logger = LoggerFactory.getLogger(KPSSQueryService.class);
+
 	@Value("${kpss.endpoint}")
 	private String kpssEndPoint;
 	
@@ -30,9 +29,8 @@ public class KPSSQueryService {
 		uriParams.put("tcno", tcno);
 		//ResponseEntity<ExamResult> examResultRE = restTemplate.getForEntity("http://3.9.117.186:7777/kpss/sinavSonucu?tcno=" + tcno, ExamResult.class);
 		ExamResult[] examResultREArray = restTemplate.getForObject("http://" + kpssEndPoint + ":" + kpssPort + "/kpss/sinavSonucu?tcno={tcno}", ExamResult[].class, uriParams);
+		logger.info(String.format("Received %d exam results from KPSS service.", examResultREArray == null ? 0 : examResultREArray.length));
 		
-		for (ExamResult er : examResultREArray)
-			System.out.println("ExamResult: " + er);
 		return examResultREArray;
 	}
 }
